256779eb27ef00b1223d0263b0ce157405465361
moizsiraj <abdulmoiz68@gmail.com>
2020-04-15 10:43:56 +0500

    Project Complete

--- script.js
+++ script.js
@@ -5,16 +5,30 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 let ticketCost = +movieSelector.value;
 
+populateUI();
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("movieSelected", movieIndex);
+  localStorage.setItem("moviePrice", moviePrice);
+}
+
 function updateCountTotal() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   const totalSelectedSeats = selectedSeats.length;
   let totalCost = totalSelectedSeats * ticketCost;
   count.innerText = totalSelectedSeats;
   total.innerText = totalCost;
+
+  const seatIndex = [...selectedSeats].map(function (seat) {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatIndex));
 }
 
 movieSelector.addEventListener("change", function (e) {
   ticketCost = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateCountTotal();
 });
 
@@ -27,3 +41,26 @@ container.addEventListener("click", function (e) {
     updateCountTotal();
   }
 });
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovie = localStorage.getItem("movieSelected");
+  if (selectedMovie !== null) {
+    movieSelector.selectedIndex = selectedMovie;
+  }
+
+  const moviePrice = localStorage.getItem("moviePrice");
+  if (moviePrice !== null) {
+    ticketCost = moviePrice;
+  }
+}
+
+updateCountTotal();
